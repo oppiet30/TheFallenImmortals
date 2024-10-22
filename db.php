369bc69b4f545e830c567753c03b9bccd6712632
Robert Hinson <oppiet35@yahoo.com>
2024-10-21 23:34:07 -0400

    Update db.php
    Changed mysql to mysqli, there is nothing wrong with mysqli.
    Unless you want to be able to connect to other database engines.

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ $database = "homestead";
 $dbuser = "homestead";
 $dbpass = "secret";
 
-$login = mysql_connect($dbhost, $dbuser, $dbpass) or trigger_error(mysql_error(),E_USER_ERROR);
-mysql_select_db($database) or die("Where?");
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or trigger_error(mysql_error(),E_USER_ERROR);
+mysqli_select_db($conn, $database) or die("Where?");
 include('varset.php');
 
 //Global Variables
@@ -15,23 +15,23 @@ $date = time();
 
 //Auto Administration
 //Banned
-$getbanned = mysql_query("SELECT * FROM banned WHERE ip='".$charip."'");
-if(mysql_num_rows($getbanned) == "1")
+$getbanned = mysqli_query($conn, "SELECT * FROM banned WHERE ip='".$charip."'");
+if(mysqli_num_rows($getbanned) == "1")
 {
     print("alert('You are banned.');");
     print("window.location = 'http://fallenimmortals.old/';");
 }
 
 //Unmute
-$getmuted = mysql_query("SELECT * FROM muted");
-while($muted = mysql_fetch_array($getmuted))
+$getmuted = mysqli_query($conn, "SELECT * FROM muted");
+while($muted = mysqli_fetch_array($getmuted))
 {
     if($muted['mutetime'] <= time())
     {
-        $unmute = mysql_query("DELETE FROM muted WHERE id='".$muted['id']."'");
+        $unmute = mysqli_query($conn, "DELETE FROM muted WHERE id='".$muted['id']."'");
 
         $unmutemessage = "<b><font color=\'#DD00DD\'>Player ".$muted['username']." has been unmuted!</font></b><br />";
-        $query = mysql_query("INSERT INTO chatroom (`date`, `userlevel`, `username`, `message`, `to`)
+        $query = mysqli_query($conn, "INSERT INTO chatroom (`date`, `userlevel`, `username`, `message`, `to`)
         VALUES ('".$date."', '3', '".$muted['mutedby']."', '".$unmutemessage."', 'Chatroom')") or die(mysql_error());
     }
 }
