28fa8bbc200e3132ba2c7100829f3dd5f7d19e6f
Robert Hinson <oppiet35@yahoo.com>
2024-10-23 12:06:24 -0400

    Fixed the includes.

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
 //I've set this up to make the connection to your db, shouldn't need changing unless i got something wrong
-include db_include.php;
+require_once "db_include.php";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $database) or trigger_error(mysqli_error($conn),E_USER_ERROR);
 mysqli_select_db($conn, $database) or die("Where?");
